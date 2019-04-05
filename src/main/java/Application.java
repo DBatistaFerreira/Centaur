f9d8855784421cae0829a322678c3c76345dca57
clearedkinkajou <bedardjake@gmail.com>
2019-04-05 11:50:11 -0400

    Started changes to two point calculations

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,17 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Application {
 
-    private static final double EARTHS_RADIUS = 6378137;
-    private static final double RADIUS_OF_SEARCH_LATITUDE = 500; // meters
-    private static final double RADIUS_OF_SEARCH_LONGITUDE = 250; // meters
+    private static final double EARTHS_RADIUS = 6378137.0;
+    private static final double RADIUS_OF_SEARCH_LATITUDE = 500.0; // meters
+    private static final double RADIUS_OF_SEARCH_LONGITUDE = 250.0; // meters
     private static PlaceService placeService = new PlaceService();
 
-    private static FileOutputStream fos;
-    private static ObjectOutputStream oos;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //HashMap<String, Place> places = placeService.getPlacesFromCoordinates("45.485340","-73.621447");
 
         HashMap<String, Place> places = getAllWestmountRestaurants();
@@ -23,18 +19,15 @@ public class Application {
         System.out.println("Amount of restaurants: " + places.size());
     }
 
+
     //Westmount has specifically Westmount and not montreal in their address
     public static HashMap<String, Place> getAllWestmountRestaurants(){
 
         //Places all of Westmount into a Square
-        final double TOP_LEFT_LATITUDE = 45.485340;
-        final double TOP_LEFT_LONGITUDE = -73.621447;
-        final double TOP_RIGHT_LATITUDE = 45.496558;
-        final double TOP_RIGHT_LONGITUDE = -73.621447;
-        final double BOTTOM_LEFT_LATITUDE = 45.473787;
-        final double BOTTOM_LEFT_LONGITUDE = -73.597991;
-        final double BOTTOM_RIGHT_LATITUDE = 45.485031;
-        final double BOTTOM_RIGHT_LONGITUDE = -73.597991;
+        final double TOP_LEFT_LATITUDE = 45.495642; //x1
+        final double TOP_LEFT_LONGITUDE = -73.618438; //y1
+        final double BOTTOM_RIGHT_LATITUDE = 45.473589; //x2
+        final double BOTTOM_RIGHT_LONGITUDE = -73.580635; //y2
 
         double currentLatitude = TOP_LEFT_LATITUDE;
         double currentLongitude = TOP_LEFT_LONGITUDE;
@@ -45,9 +38,9 @@ public class Application {
         //Get initial position restaurants
         HashMap<String, Place> places = placeService.getPlacesFromCoordinates(Double.toString(TOP_LEFT_LATITUDE), Double.toString(TOP_LEFT_LONGITUDE));
 
-        double currentLatitudeBounds = TOP_RIGHT_LATITUDE;
+        double currentLatitudeBounds = BOTTOM_RIGHT_LATITUDE;
 
-        while(currentLongitude < BOTTOM_LEFT_LONGITUDE){
+        while(currentLongitude < BOTTOM_RIGHT_LONGITUDE){
             while(currentLatitude < currentLatitudeBounds){
                 currentLatitude = nextLatitude;
                 places.putAll(placeService.getPlacesFromCoordinates(Double.toString(currentLatitude), Double.toString(currentLongitude)));
@@ -62,7 +55,7 @@ public class Application {
         Iterator it = places.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry<String,Place> pair = (Map.Entry)it.next();
-            if(!pair.getValue().getAddress().toLowerCase().contains("westmount")){
+            if(pair.getValue().getAddress().contains("Montréal") || pair.getValue().getAddress().contains("Montreal")){
                 it.remove();
             }
         }
@@ -74,12 +67,12 @@ public class Application {
     //Source for the following formulas
     private static double getLatitudeOffSet(double latitude, double radius){
         double dlat = radius/EARTHS_RADIUS;
-        return latitude + (dlat * 180/Math.PI);
+        return latitude + (dlat * 180.0/Math.PI);
     }
 
     private static double getLongitudeOffSet(double longitude, double latitude, double radius){
-        double dlon = radius/(EARTHS_RADIUS * Math.cos(Math.PI * latitude/180));
-        double newLongitude = longitude + (dlon * 180/Math.PI);
+        double dlon = (radius/EARTHS_RADIUS) * (180.0/Math.PI / Math.cos((Math.PI * latitude)/180.0));
+        double newLongitude = longitude + dlon;
         return newLongitude;
 }
 }
